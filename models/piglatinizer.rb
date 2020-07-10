d607c050f3c442ebed2c5da1c4cff3955eed574d
Nabil Hayet <nabil.bracu@gmail.com>
2020-07-10 19:29:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,7 +17,6 @@ class PigLatinizer
               index = value.index(/[aeiouAEIOU]/)
               val = value.slice!(0..index-1)
               result = value + val + "ay"
-              results[index] = result
             end
         end
         results
